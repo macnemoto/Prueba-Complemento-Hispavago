79f4f58897733b5791a1b8b10b105e82c0fc80cc
Marcos <marcosj465@gmail.com>
2020-06-22 23:33:20 -0400

    Logica de scraping y limpieza abanzada

--- logica.js
+++ logica.js
@@ -6,17 +6,23 @@ const request = require('request-promise');
 async function nekoMinig() {
 
 const $ = await request({
-uri: 'https://www.hispachan.org/ve/res/1700446.html',
+uri: 'https://www.hispachan.org/ve/res/1701249.html',
 transform: body => cheerio.load(body)
 
 })
 let reGex1 = /( VE ........ ........ ..:.. \/#\/ |>>..*|>>....... \(OP\)|>>.......|\/#\/ ....... \[X\]|\d{7}|\d{5}|.*png..|.*.....KB ,.*...x....*,|.*jpg..)/gi;
 let reGex2 =/^\s+|\s+$|\s+(?=\s)/g
+let reGex3 =/(\[X\]|\/#\/|>>.........*|\[★\]|\[S\]|\[A\]|...*.jpg|...*.png|...*.gif|...*.mp4|...*.webm)/gi
+let reGex4 =/(.jpg|.png|.webm|.mp4|.gif|\(.....................)/gis
+let reGex5 =/(Eliminar..*CP)/gis
 let titulo = $('#delform');
 titulo = titulo.text();
 //titulo = (titulo.replace(reGex1, ""));
 titulo = titulo.replace(reGex1, "").trim()
-console.log(titulo.replace(reGex2, ""))
+titulo = titulo.replace(reGex2, "")
+titulo = titulo.replace(reGex3, "")
+titulo = titulo.replace(reGex4, "")
+console.log(titulo.replace(reGex5, ""))
 
 
 }
